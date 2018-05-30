9baaa46caf6f706df01f4e411138745d2154bf5d
Kurt <kurtbauer55@protonmail.com>
2018-05-30 16:45:28 -0400

    full iteration still broken

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,26 +4,32 @@ require_relative '../app/models/character.rb'
 require_relative '../app/models/title.rb'
 require_relative '../app/models/house.rb'
 require 'pry'
+require 'json'
 
-houses_url = "https://anapioficeandfire.com/api/houses"
-characters_url = "https://anapioficeandfire.com/api/characters"
+# houses_url = "https://anapioficeandfire.com/api/houses"
+# characters_url = "https://anapioficeandfire.com/api/characters"
 
-response = HTTParty.get(characters_url)
-parsed_response = response.parsed_response
+file = File.read("../lib/got_chars.json")
+hash = JSON.parse(file)
+# characters_url = "./got_chars.json"
+# new_j = JSON.parse(characters_url)
+# parsed_response = response.parsed_response
 
+  def make_character_array(hash)
+    hash.select do |character|
+      name = character["Name"]
+      gender = character["Gender"]
+      culture = character["Culture"]
+      born = character["Born"]
+      died = character["Died"]
 
-  def make_character_array(parsed_response)
-    parsed_response.each do |individual_character|
-      # binding.pry
-      Character.create(name: "jon snow")
-      true
+      char = Character.new(name: name, gender: gender, culture: culture, born: born, died: died  )
+      a_char.save
     end
   end
 
-  make_character_array(parsed_response)
-
-
-
+  make_character_array(hash)
   # require_all 'lib'
   # require_all 'app'
   # require 'httparty'
+pry.Start
