f4bdb288382d298abe5f5db4a8f985313f05323b
github username <github email address>
2018-05-30 23:04:44 -0400

    seeded house database

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,9 @@ characters_url = "https://anapioficeandfire.com/api/characters?pageSize=25"
 response = HTTParty.get(characters_url)
 parsed_response = response.parsed_response
 
+house_response = HTTParty.get(houses_url)
+house_parsed_response = house_response.parsed_response
+
 
   def make_character_array(parsed_response)
     parsed_response.each do |individual_character|
@@ -48,8 +51,18 @@ parsed_response = response.parsed_response
     end
   end
 
+
+
+  def make_house_array(house_parsed_response)
+    house_parsed_response.each do |individual_house|
+      House.create(name: individual_house["name"], region: individual_house["region"], coatOfArms: individual_house["coatOfArms"], words: individual_house["words"], currentLord: individual_house["currentLord"])
+
+    end
+  end
+
   make_character_bio_array(parsed_response)
   make_character_nickname_array(parsed_response)
+  make_house_array(house_parsed_response)
 
 
 
